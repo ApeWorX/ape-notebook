d1c595c69ead856d93d66186fc225312901c1b10
johnson2427 <blakeejohnson39@gmail.com>
2022-05-13 15:40:34 -0500

    feat: added the start of building the kernel to override IPython

---
+++ ape_notebook/apekernel.py
@@ -0,0 +1,29 @@
+from ipykernel.kernelbase import Kernel
+
+
+class ApeKernel(Kernel):
+    implementation = "Ape"
+    implementation_version = "1.0"
+    language = "no-op"
+    language_version = "0.1"
+
+
+    banner = "Echo kernel - as useful as a parrot"
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        if not silent:
+            stream_content = {'name': 'stdout', 'text': code}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok',
+                # The base class increments the execution count
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+                }
+
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=ApeKernel)
