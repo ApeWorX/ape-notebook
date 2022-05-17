a3287579f7546e690c75c8d6bfbde30f151dffc7
johnson2427 <blakeejohnson39@gmail.com>
2022-05-17 10:48:32 -0500

    feat: adding subprocess to build kernel, missing something here at the moment

--- ape_notebook/_cli.py
+++ ape_notebook/_cli.py
@@ -1,7 +1,17 @@
 import click
 from notebook import notebookapp as app
+import subprocess
+
+from ape.cli import NetworkBoundCommand, ape_cli_context, network_option
 
 
-@click.command(short_help="Run a notebook server")
-def cli():
+@click.command(
+    cls=NetworkBoundCommand,
+    short_help="Run a notebook server"
+)
+@network_option()
+@ape_cli_context()
+def cli(cli_ctx, network):
+    args = ["ipython", "kernel", "install", "--name", "ape", "--user"]
+    subprocess.run(args)
     app.launch_new_instance()
