8c44866eca2133f3bbc54d12b70ee1caee167a56
Mihai <mcosma@gmail.com>
2023-03-28 14:59:28 -0400

    fix mypy

--- ape_notebook/_cli.py
+++ ape_notebook/_cli.py
@@ -1,8 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: elf-env
+#     language: python
+#     name: python3
+# ---
+
+# %%
 import click
 
-from notebook import notebookapp as app
+# %%
+import notebook
 
 
+# %%
 @click.command(short_help="Run a notebook server")
 def cli():
-    app.launch_new_instance()
+    notebook.notebookapp.app.launch_new_instance()
+
+# %%
